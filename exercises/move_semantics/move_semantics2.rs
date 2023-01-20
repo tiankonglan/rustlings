7305d1dd91025d32923302589e484d47490457d6
denny.liu <denny.liu@wetax.com.cn>
2023-01-20 14:11:26 +0800

    exersie commit

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,7 +2,6 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn main() {
     let vec0 = Vec::new();
@@ -10,7 +9,7 @@ fn main() {
     let mut vec1 = fill_vec(vec0);
 
     // Do not change the following line!
-    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+    // println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
     vec1.push(88);
 
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,17 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(price : u32) -> u32 {
+    if price <= 40 {
+        return price * 2
+    } else {
+        price
+    }
+    
 
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
